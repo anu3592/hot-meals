26cec7933752dc0fa552075b0e66ebe80466c30b
anu3592 <103917599+anu3592@users.noreply.github.com>
2024-04-18 21:40:50 +0530

    Add files via upload

---
+++ Frontend/src/data/data.js
@@ -0,0 +1,245 @@
+import icon1 from '../img/placedicon.png';
+import icon2 from '../img/preparingicon.jpg';
+import icon3 from '../img/pickedicon.jpg';
+import icon4 from '../img/completeicon.png';
+import img1 from '../img/placed.jpg';
+import img2 from '../img/preparing.jpg';
+import img3 from '../img/picked.jpg';
+import img4 from '../img/complete.jpg';
+
+const icn1 = icon1;
+const icn2 = icon2;
+const icn3 = icon3;
+const icn4 = icon4;
+
+export const topPicks = [
+    {
+        id: 1,
+        title: 'Chole Bhature',
+        price: 'Rs 180',
+        img: 'https://content.jdmagicbox.com/comp/noida/v8/011pxx11.xx11.190925042619.e8v8/catalogue/punjabi-chole-bhature-noida-sector-34-noida-north-indian-restaurants-2whbntunqe.jpg?clr=#662200'
+    },
+    {
+        id: 2,
+        title: 'Pav Bhaji',
+        price: 'Rs 120',
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZ3fO1Skulriv4Vqt0I798Tnwcax2qTuoEjQ&usqp=CAU'
+    },
+    {
+        id: 3,
+        title: 'Dosa',
+        price: 'Rs 280',
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4aR5MbjKV-FVPSIrUrHXXpYK9BCHlL8Q1BQ&usqp=CAU'
+    },
+    {
+        id: 4,
+        title: 'Burger',
+        price: 'Rs 100',
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHVQltN1opACTC3TdMorCu_Zc0WZxWVtJfqg&usqp=CAU'
+    },
+    {
+        id: 5,
+        title: 'Pizza',
+        price: 'Rs 190',
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQJqEMksF4A3izAQMMFpEsF3PI-xm2jWEGmA&usqp=CAU'
+    },
+    {
+        id: 6,
+        title: 'Kadhai Paneer',
+        price: 'Rs 150',
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRa5aeMU7cuCZKKK_GpcRYySs_OLv47L-kN8feUPY2f69B85VyF8eq15M4YcTOdAMKtKWQ&usqp=CAU'
+    },
+    {
+        id: 7,
+        title: 'Dahi Bhalle',
+        price: 'Rs 90',
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0cN731bIWoJNwLn1wH4VvDf9LMBXwd5O5Xg&usqp=CAU'
+    },
+    {
+        id: 8,
+        title: 'Pulaw',
+        price: 'Rs 100',
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3tjNnHDoQVDPoAaHnrQku9TZssgwLT31R7g&usqp=CAU'
+    }
+]
+
+export const mealData = [
+    {
+        id: 1,
+        name: "cheeze Pizza",
+        category: "Pizza",
+        price: "Rs 200",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGSiCfs8q26ZPMZJhYo84dTADSLje-pJaqiA&usqp=CAU"
+    },
+    {
+        id: 2,
+        name: "veggie Pizza",
+        category: "Pizza",
+        price: "Rs 250",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRm-echCkGuKpMaMtnBTG_1x4S-BaJVoqdw8w&usqp=CAU"
+    },
+    {
+        id: 3,
+        name: "pepperoni Pizza",
+        category: "Pizza",
+        price: "Rs 280",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlgyrcAH9iKC-xQWfL8SYBKCT6MSsyq0oTQw&usqp=CAU"
+    },
+    {
+        id: 4,
+        name: "margherita Pizza",
+        category: "Pizza",
+        price: "Rs 1000",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPVKRyYZd-2Uzkavrzkbw8aZgzHOYqicnXBA&usqp=CAU"
+    },
+    {
+        id: 5,
+        name: "mango Shake",
+        category: "Shake",
+        price: "Rs 150",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7OBBWBwCEqwGwkXTEosIXvWJoBwrgfxAZpg&usqp=CAU"
+    },
+    {
+        id: 6,
+        name: "banana Shake",
+        category: "Shake",
+        price: "Rs 160",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVLtHEKENSrno0W4WnYL8XPzgFe0u70K_Z4Q&usqp=CAU"
+    },
+    {
+        id: 7,
+        name: "oreo Shake",
+        category: "Shake", 
+        price: "Rs 130",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgTNJFK1EAf0ZgREG7HidtdLeR6wYaIQ4Qag&usqp=CAU"
+    },
+    {
+        id: 8,
+        name: "strawberry Shake",
+        category: "Shake", 
+        price: "Rs 140",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGJoCFy8X3jGqPPWLulj3EpTsD0BHzcOyyUw&usqp=CAU"
+    },
+    {
+        id: 9,
+        name: "gulab Jamun",
+        category: "Sweet", 
+        price: "Rs 120",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShPmCqqNda8PSgYpyPzuMOaTawxo3b-mviZA&usqp=CAU"
+    },
+    {
+        id: 10,
+        name: "rasmalai",
+        category: "Sweet", 
+        price: "Rs 200",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvKFIl1kgy6XWPVrDxGDy1ZYWIz8cTX2Wfrg&usqp=CAU"
+    },
+    {
+        id: 11,
+        name: "kulfi",
+        category: "Sweet", 
+        price: "Rs 100",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-Q0JXuKcXoP_DVydg9c1PSrtMRffBAvj3XA&usqp=CAU"
+    },
+    {
+        id: 12,
+        name: "softy",
+        category: "Sweet", 
+        price: "Rs 140",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ57WrcxqI6CuWHqzHsEWwlxY3JcKX303jRMQ&usqp=CAU"
+    }
+
+]
+
+export const category = [
+    {
+        id: 1,
+        name: "Burger",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0jEr5OWcVEJ-Bxnz_tAdPs31LKg5DB99jYQ&usqp=CAU"
+    },
+    {
+        id: 2,
+        name: "Pizza",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkSbW_vwpSh8KI-3WIeCHxSWr1azWKqJx_WQ&usqp=CAU"
+    },
+    {
+        id: 3,
+        name: "Veg",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2kF193THDkQMPY2Iq34fuMGZCehLJdk4MIA&usqp=CAU"
+    },
+    {
+        id: 4,
+        name: "Sweets",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhKDz4KkRxM3AkSgSyJHgHNlfqmipZNlzQ9Q&usqp=CAU"
+    },
+    {
+        id: 5,
+        name: "Noodles",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSK1vyzFEGwsMk4y3IrFBvbNRBJT-GU0iLH8w&usqp=CAU"
+    },
+    {
+        id: 6,
+        name: "Ice Cream",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJrqihCw8xCrj6KD7gHmZ2ZRus4ACBmHyhnA&usqp=CAU"
+    },
+    {
+        id: 7,
+        name: "Juice",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqCzophDcIzfq800mr8fN9p3HM88QobiiXQw&usqp=CAU"
+    },
+    {
+        id: 8,
+        name: "Rolls",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDtQuU85Dy2GVeMYYEt5-R8fwaqtOnL3qrnw&usqp=CAU"
+    }
+]
+
+export const orderStatus = [
+    {
+        id: 1,
+        name: "placed",
+        //img: "../img/placedicon.png"
+        img:icn1
+    },
+    {
+        id: 2,
+        name:"preparing",
+        //img: "../img/preparingicon.jpg"
+        img:icn2
+    },
+    {
+        id: 3,
+        name: "picked",
+        //img: "../img/pickedicon.jpg"
+        img:icn3
+    },
+    {
+        id:4,
+        name: "complete",
+        //img: "../img/completeicon.png"
+        img:icn4
+    }
+]
+
+export const orderStatusImage = [
+    {
+        id: 1,
+        //img: "../img/placedicon.png"
+        img:img1
+    },
+    {
+        id: 2,
+        //img: "../img/preparingicon.jpg"
+        img:img2
+    },
+    {
+        id: 3,
+        //img: "../img/pickedicon.jpg"
+        img:img3
+    },
+    {
+        id:4,
+        //img: "../img/completeicon.png"
+        img:img4
+    }
+]
